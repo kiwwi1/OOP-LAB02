462dd75b1c28ab6b7dca4a621a8570b1ccb6d7f1
kiwwi1 <dquanghuy221@gmail.com>
2024-11-23 08:07:21 +0700

    Lab02

---
+++ aimsproject/Cart.java
@@ -0,0 +1,58 @@
+package lab002.aimsproject;
+import lab002.aimsproject.DigitalVideoDisc;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cart {
+    public static final int MAX_NUMBERS_ORDERED = 20;
+    private int qtyOrdered = 0;
+    private DigitalVideoDisc itemsOrdered[] = new DigitalVideoDisc[MAX_NUMBERS_ORDERED];
+    public int getQtyOrdered() {
+        return qtyOrdered;
+    }
+    public void addDigitalVideoDisc(DigitalVideoDisc item) {
+        if(qtyOrdered < MAX_NUMBERS_ORDERED) {
+            itemsOrdered[qtyOrdered] = item;
+            qtyOrdered++;
+            System.out.println("The disc has been added");
+        }
+        else{
+            System.out.println("The cart is full");
+        }
+    }
+    public void removeDigitalVideoDisc(DigitalVideoDisc item) {
+        for(int i = 0; i < qtyOrdered; i++) {
+            if(itemsOrdered[i] == item) {
+                for(int j = i; j < qtyOrdered-1; j++) {
+                    itemsOrdered[j] = itemsOrdered[j+1];
+                }
+                qtyOrdered--;
+                itemsOrdered[qtyOrdered] = null;
+            }
+        }
+        System.out.println("The disc has been removed");
+    }
+    public float totalCost(){
+        float total = 0;
+        for(int i = 0; i < qtyOrdered; i++) {
+            total += itemsOrdered[i].getCost();
+        }
+        return total;
+    }
+
+    public void addDigitalVideoDisc(DigitalVideoDisc[] dvdList) {
+        for (DigitalVideoDisc dvd : dvdList) {
+            if (dvd == null) continue;
+            if (qtyOrdered < MAX_NUMBERS_ORDERED) {
+                itemsOrdered[qtyOrdered] = dvd;
+                qtyOrdered++;
+                System.out.println("The disc has been added: " + dvd.getTitle());
+            } else {
+                System.out.println("The cart is full, cannot add: " + dvd.getTitle());
+                break;
+            }
+        }
+    }
+
+
+}
